d9199fd66f7ae9f9ed1af6bfb25e81c1bc5a91b5
Felipe <felipee_sc@hotmail.com>
2026-04-27 13:53:29 -0300

    add ECR repository

---
+++ terraform/ecr.tf
@@ -0,0 +1,27 @@
+resource "aws_ecr_repository" "app" {
+  name                 = var.project
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+
+  tags = local.common_tags
+}
+
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = aws_ecr_repository.app.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Manter apenas as 10 imagens mais recentes"
+      selection = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 10
+      }
+      action = { type = "expire" }
+    }]
+  })
+}
